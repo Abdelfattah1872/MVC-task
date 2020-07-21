397a978a38ec21fc68ec470a85bf2fa391f57ab0
abdelfatah <mohamed.araby321999@gmail.com>
2020-07-21 12:21:37 +0200

    more

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ Auth::routes();
 #################### only Register user ####################################
 Route::group(['middleware'=>'auth'], function () {
     Route::get('/','PostController@index');
+    Route::get('/home','PostController@index');
     Route::resource('user', 'UserController');
     Route::resource('post', 'PostController');
     Route::get('above', 'PostController@above');
